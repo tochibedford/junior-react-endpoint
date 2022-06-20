d78fee31e0f0e168e807924025665cb8fe2fd4ca
Tochukwu Bedford <34871260+tochibedford@users.noreply.github.com>
2022-06-20 03:08:08 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const server = new ApolloServer({
     resolvers: resolvers
 });
 
-server.listen().then(({ url }) => {
+server.listen({port: process.env.PORT || 5000}).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
